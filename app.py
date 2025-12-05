9314a311646e7e10badcb8f9cbda1dcceeb85819
salman <103653847+mhd-salm@users.noreply.github.com>
2025-12-05 10:10:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -274,6 +274,8 @@ def reload_pdfs():
 
 # --------------- RUN -----------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=APP_PORT)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
    # app.run(port=4000)
+
 
